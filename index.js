acd0416bfdf8a63d04e28b3c56a42a6721ae3932
vivaxy <xyxuye2007@126.com>
2018-09-17 14:40:44 +0800

    chore:

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-const { app, BrowserWindow } = require('electron');
+/**
+ * @since 20180917 13:42
+ * @author vivaxy
+ */
 
-let win;
-function createWindow() {
-  // 创建浏览器窗口
-  win = new BrowserWindow({ width: 800, height: 600 });
+(function() {
+  const path = require('path');
+  const amdLoader = require('./node_modules/monaco-editor/min/vs/loader.js');
+  const amdRequire = amdLoader.require;
+  const amdDefine = amdLoader.require.define;
 
-  // 然后加载应用的 index.html。
-  win.loadFile('index.html');
-}
+  function uriFromPath(_path) {
+    var pathName = path.resolve(_path).replace(/\\/g, '/');
+    if (pathName.length > 0 && pathName.charAt(0) !== '/') {
+      pathName = '/' + pathName;
+    }
+    return encodeURI('file://' + pathName);
+  }
 
-app.on('ready', createWindow);
+  amdRequire.config({
+    baseUrl: uriFromPath(path.join(__dirname, './node_modules/monaco-editor/min')),
+  });
+  // workaround monaco-css not understanding the environment
+  self.module = undefined;
+  amdRequire(['vs/editor/editor.main'], function() {
+    var editor = monaco.editor.create(document.getElementById('container'), {
+      value: [
+        'function x() {',
+        '\tconsole.log("Hello world!");',
+        '}',
+      ].join('\n'),
+      language: 'javascript',
+    });
+  });
+})();
